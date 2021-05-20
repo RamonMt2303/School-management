b5e3c72be50cd7e79f04fb867359ccc01d757a17
Ramón Martínez Delgado <69860663+RamonMt2303@users.noreply.github.com>
2021-05-20 11:08:10 -0500

    Add files via upload

--- Universidad.h
+++ Universidad.h
@@ -28,23 +28,17 @@ public:
         this -> registro = _registro;
     }
     void llenarDatos(){
-    int registro;
-    cout << "¿Cuántas personas desea registrar?" << endl;
-    cin >> registro;
-    Persona *total[registro];
-    for(int i = 0; i < registro; i++){
         int confirmacion;
-        cout << "La persona que va a registrar, ¿A que grupo pertenece?" << endl;
-        cout << "1 para Alumno, 2 para profesor y 3 para otros" << endl;
+        cout << "¿A qué grupo quiere acceder para hacer un registro?" << endl;
+        cout << "1 para Alumnos, 2 para Profesores y 3 para Personal de Limpieza" << endl;
         cin >> confirmacion;
         if (confirmacion == 1){
-            int registrarAlumno;
-            cout << "Ingrese 1 para añadir un nuevo alumno a la lista de alumnos, \
-            y a la lista general de la universidad o 0 para salir y acceder a otro grupo" << endl;
-            cin >> registrarAlumno;
-            Persona *Alumnos[registrarAlumno];
-            while(registrarAlumno != 0){
-                for (int i=0;i<registrarAlumno;i++){
+            int registroAlumno;
+            int confirmacionImpresion;
+        cout << "¿Cuántos Alumnos desea registrar?" << endl;
+            cin >> registroAlumno;
+            Persona *Alumnos[registroAlumno];
+                for (int i=0;i<registroAlumno;i++){
                     string nombre;
                     int edad;
                     string carrera;
@@ -60,21 +54,25 @@ public:
                     cin >> promedio;
                     cout << "¿En qué semestre está actualmente?" << endl;
                     cin >> nivelEstudio;
-                    total[i] = new Alumno(nombre, edad, carrera, promedio, nivelEstudio);
                     Alumnos[i] = new Alumno(nombre, edad, carrera, promedio, nivelEstudio);
-                    cout << "Ingrese 1 para añadir un nuevo alumno a la lista de alumnos, \
-                    y a la lista general de la universidad o 0 para salir y acceder a otro grupo" << endl;
-                    cin >> registrarAlumno;
                 }
+                    cout << "¿Desea imprimir la lista de los Estudiantes?, Ingrese 1 para si, o 0 para no hacerlo" << endl;
+                    cin >> confirmacionImpresion;
+                    if (confirmacionImpresion == 1){
+                        for (int i = 0; i<registroAlumno;i++){
+                            Alumnos[i]->mostrar();
+                            cout << "\n" << endl;
+                                    }
+                    }else{
+                            cout << "Cancelando impresión del personal" << endl;
+                                }
             }
-        }
-            else if (confirmacion == 2){
-                int registrarProfesor;
-                cout << "Ingrese 1 para añadir un nuevo profesor a la lista de profesores, \
-                y a la lista general de la universidad o 0 para salir y acceder a otro grupo" << endl;
+        else if (confirmacion == 2){
+            int registrarProfesor;
+            int confirmacionImpresion;
+                cout << "¿Cuántos profesores desea registrar?" << endl;
                 cin >> registrarProfesor;
                 Persona *Profesores[registrarProfesor];
-                while(registrarProfesor != 0){
                     for (int i=0;i<registrarProfesor;i++){
                         string nombre;
                         int edad;
@@ -97,55 +95,61 @@ public:
                         cin >> especialidad;
                         cout << "¿Cuál es su número telefónico?" << endl;
                         cin >> celular;
-                        total[i] = new Profesor(nombre, edad, materia, salario, grupos, especialidad, celular);
-                        Profesores[i] = new Profesor(nombre, edad, materia, salario, grupos, especialidad, celular);
-                        cout << "Ingrese 1 para añadir un nuevo profesor a la lista de profesores, \
-                        y a la lista general de la universidad o 0 para salir y acceder a otro grupo" << endl;
-                        cin >> registrarProfesor;
+                       Profesores[i] = new Profesor(nombre, edad, materia, salario, grupos, especialidad, celular);
                     }
+                        cout << "¿Desea imprimir la lista solo de los Profesores?, Ingrese 1 para si, o 0 para no hacerlo" << endl;
+                        cin >> confirmacionImpresion;
+                        if (confirmacionImpresion == 1){
+                            for (int i = 0; i<registrarProfesor;i++){
+                                Profesores[i]->mostrar();
+                                cout << "\n" << endl;
+                                        }
+                        }else{
+                                cout << "Cancelando impresión del personal" << endl;
+                                    }
                 }
+        else if (confirmacion == 3){
+            int registrarLimpieza;
+            int confirmacionImpresion;
+            cout << "¿Cuánto personal de Limpieza desea registrar?" << endl;
+            cin >> registrarLimpieza;
+            Persona *personalLimpieza[registrarLimpieza];
+            for (int i=0;i<registrarLimpieza;i++){
+                string nombre;
+                int edad;
+                string actividades;
+                float salario;
+                int salones;
+                string celular;
+                string estudios;
+                cout << "¿Cuál es el nombre del encargado de limpieza?" << endl;
+                cin >> nombre;
+                cout << "¿Cuál es su edad?" << endl;
+                cin >> edad;
+                cout << "¿Qué actividad se encarga de realiza?" << endl;
+                cin.ignore();
+                getline(cin, actividades);
+                cout << "¿Cuál es su salario?" << endl;
+                cin >> salario;
+                cout << "¿Cuántos salones se encarga de limpiar?" << endl;
+                cin >> salones;
+                cout << "¿Qué nivel de estudios tiene? (Secundaria, Preparatoria, etc)" << endl;
+                cin >> estudios;
+                cout << "¿Cuál es su número telefónico?" << endl;
+                cin >> celular;
+                personalLimpieza[i] = new Limpieza(nombre, edad, actividades, salario, salones, celular, estudios);
             }
-            else if (confirmacion == 3){
-                int registrarLimpieza;
-                cout << "Ingrese 1 para añadir un nuevo personal de limpieza a la lista de limpieza, \
-                y a la lista general de la universidad o 0 para salir y acceder a otro grupo" << endl;
-                cin >> registrarLimpieza;
-                Persona *personalLimpieza[registrarLimpieza];
-                while(registrarLimpieza != 0){
-                    for (int i=0;i<registrarLimpieza;i++){
-                        string nombre;
-                        int edad;
-                        string actividades;
-                        float salario;
-                        int salones;
-                        string celular;
-                        string estudios;
-                        cout << "¿Cuál es el nombre del encargado de limpieza?" << endl;
-                        cin >> nombre;
-                        cout << "¿Cuál es su edad?" << endl;
-                        cin >> edad;
-                        cout << "¿Qué actividad se encarga de realiza?" << endl;
-                        cin >> actividades;
-                        cout << "¿Cuál es su salario?" << endl;
-                        cin >> salario;
-                        cout << "¿Cuántos salones se encarga de limpiar?" << endl;
-                        cin >> salones;
-                        cout << "¿Qué nivel de estudios tiene? (Secundaria, Preparatoria, etc)" << endl;
-                        cin >> estudios;
-                        cout << "¿Cuál es su número telefónico?" << endl;
-                        cin >> celular;
-                        total[i] = new Limpieza(nombre, edad, actividades, salario, salones, celular, estudios);
-                        personalLimpieza[i] = new Limpieza(nombre, edad, actividades, salario, salones, celular, estudios);
-                        cout << "Ingrese 1 para añadir un nuevo profesor a la lista de limpieza, \
-                        y a la lista general de la universidad o 0 para salir y acceder a otro grupo" << endl;
-                        cin >> registrarLimpieza;
+                cout << "¿Desea imprimir la lista solo de los empleados del área de limpieza?                   Ingrese 1 para si, o 0 para no hacerlo" << endl;
+                cin >> confirmacionImpresion;
+                if (confirmacionImpresion == 1){
+                    for (int i = 0; i<registrarLimpieza;i++){
+                        personalLimpieza[i]->mostrar();
+                        cout << "\n" << endl;
+                                }
+                }else{
+                        cout << "Cancelando impresión del personal" << endl;
+                            }
+                        }
                     }
-                }
-            }
-        cout << "La persona que va a registrar, ¿A que grupo pertenece?" << endl;
-        cout << "1 para Alumno, 2 para profesor y 3 para otros" << endl;
-        cin >> confirmacion;
-        }
-    }
 };
 #endif /* Universidad_h */
